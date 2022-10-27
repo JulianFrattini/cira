3885c9126183c8e8092ac5f72426cd2eba1fe8d6
Julian Frattini <julian.frattini@alice-dsl.net>
2022-10-27 14:48:51 +0200

    Added simple deserialization including child-parent relationships

--- src/data/labels.py
+++ src/data/labels.py
@@ -15,12 +15,30 @@ class Label:
     def to_dict(self) -> dict:
         pass
 
+    def base_equals(self, other) -> bool:
+        return self.name == other.name and self.begin == other.begin and self.end == other.end
+
 @dataclass
 class SubLabel(Label):
     parent: 'EventLabel' = field(default=None, init=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return super().__str__()
+
+    def __eq__(self, other) -> bool:
+        if type(other) != SubLabel:
+            return False
+
+        if (self.parent == None) != (other.parent == None):
+            return False
+        else:
+            if self.parent != None and other.parent != None:
+                parent_eq = self.parent.base_equals(other=other.parent)
+                if not parent_eq:
+                    return False
+                    
+        return self.base_equals(other)
+        
 
     def to_dict(self) -> dict:
         dictified: dict = {
@@ -86,6 +104,28 @@ class EventLabel(Label):
             'children': [child.id for child in self.children]
         }
         return dictified
+
+    def __eq__(self, other) -> bool:
+        if type(other) != EventLabel:
+            return False
+
+        # check that the children are equivalent
+        if len(self.children) != len(other.children):
+            return False
+        else:
+            # check that every child is contained in the other object
+            child_equivalents = [[oc for oc in other.children if child.base_equals(oc)] for child in self.children]
+
+            # check that every child has exactly one equivalent
+            child_equivalent_lengths_unqeual_one = [ce for ce in child_equivalents if len(ce)!=1]
+            if len(child_equivalent_lengths_unqeual_one) > 0:
+                return False
+
+            child_equivalents = set([ce[0].id for ce in child_equivalents])
+            if len(child_equivalents) != len(self.children):
+                return False
+
+        return self.base_equals(other)
     
     def __str__(self):
         return super().__str__()
@@ -94,3 +134,28 @@ class EventLabel(Label):
         children = " ".join([child.__repr__() for child in self.children])
         neighbor = f' ({self.successor.junctor} {self.successor.target.id})' if self.successor != None else ""
         return f'[{self.begin}> ({self.id}) {self.name} {neighbor}: {children} <{self.end}]'
+
+def from_dict(serialized: list[dict]) -> list[Label]:
+    labels: list[Label] = []
+
+    # differentiate the label type by the attributes of the serialized labels
+    for ser in serialized:
+        if 'children' in ser.keys():
+            labels.append(EventLabel(id=ser['id'], name=ser['name'], begin=ser['begin'], end=ser['end']))
+        else:
+            labels.append(SubLabel(id=ser['id'], name=ser['name'], begin=ser['begin'], end=ser['end']))
+
+    # connect parents with their children
+    for parent in [label for label in labels if type(label)==EventLabel]:
+        child_ids = get_serialized_label_by_id(serialized, parent.id)['children']
+        for cid in child_ids:
+            child = get_label_by_id(labels, cid)
+            parent.add_child(child)
+
+    return labels
+
+def get_serialized_label_by_id(serialized: list[dict], id: str) -> dict:
+    return [label for label in serialized if label['id']==id][0]
+
+def get_label_by_id(labels: list[Label], id: str) -> Label:
+    return [label for label in labels if label.id==id][0]
