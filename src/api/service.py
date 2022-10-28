98d09c4c232ab7445390f8114c7095a2b16a6b1b
Julian Frattini <julian.frattini@alice-dsl.net>
2022-10-28 16:43:09 +0200

    Updated fastapi service

--- src/api/service.py
+++ src/api/service.py
@@ -1,10 +1,116 @@
+from abc import abstractmethod
 
-class CiraServiceMock:
+from src.cira import CiRAConverter
+
+from src.data.labels import Label
+from src.data.labels import from_dict as labels_from_dict
+
+from src.data.graph import Graph
+from src.data.graph import from_dict as graph_from_dict
+
+from src.data.test import Suite
+
+class CiRAService:
+    @abstractmethod
+    def classify(self, sentence: str) -> tuple[bool, float]:
+        pass
+
+    @abstractmethod
+    def sentence_to_labels(self, sentence: str) -> list[dict]:
+        pass
+
+    @abstractmethod
+    def sentence_to_graph(self, sentence: str, labels: list) -> dict:
+        pass
+
+    @abstractmethod
+    def graph_to_test(self, graph) -> dict:
+        pass
+
+class CiraServiceMock(CiRAService):
     model_classification = None
     model_labeling = None
 
     def __init__(self, model_classification, model_labeling):
         self.model_classification = model_classification
         self.model_labeling = model_labeling
-    def classify(self, sentence):
+
+
+    def classify(self, sentence) -> tuple[bool, float]:
         return True, 42.0
+
+class CiRAServiceImpl(CiRAService):
+    cira = None
+
+    def __init__(self, model_classification: str, model_labeling: str, use_GPU: bool=False):
+        self.cira = CiRAConverter(
+            classifier_causal_model_path=model_classification, 
+            converter_s2l_model_path=model_labeling, 
+            use_GPU=use_GPU)
+
+    def classify(self, sentence: str) -> tuple[bool, float]:
+        """Classify a given sentence as either causal or non-causal.
+        
+        parameters:
+            sentence -- single natural language sentence
+            
+        returns:
+            causal -- True, if the sentence is considered to be causal
+            confidence -- float value between 0 and 1 representing the confidence with which the classified chose either label"""
+        causal, confidence = self.cira.classify(sentence)
+        return causal, confidence
+
+    def sentence_to_labels(self, sentence: str) -> list[dict]:
+        """Generate the causal labels for a sentence.
+        
+        parameters:
+            sentence -- single natural language sentence
+        
+        returns: list of labels serialized to dictionaries
+        """
+        labels: list[Label] = self.cira.label(sentence)
+
+        # serialize all labels and return them
+        serialized = [label.to_dict() for label in labels]
+        return serialized
+
+    def sentence_to_graph(self, sentence: str, labels: list) -> dict:
+        """Generate a cause-effect-graph from a sentence and a list of labels. If the labels are not given, they will be generated.
+
+        parameters:
+            sentence -- single natural language sentence
+            labels -- list of labels (either as true labels or dictionaries)
+
+        returns: graph serialized to a dictionary       
+        """
+        # if the labels are not provided, generate them
+        if labels is None or len(labels) == 0:
+            labels = self.cira.label(sentence)
+
+        # in case the labels are serialized, deserialize them
+        if type(labels[0]) == dict:
+            labels: list[Label] = labels_from_dict(labels)
+
+        graph = self.cira.graph(sentence, labels)
+
+        # serialize the graph and return it
+        serialized = graph.to_dict()
+        return serialized
+
+    def graph_to_test(self, graph) -> dict:
+        """Generate a test suite from a cause-effect graph.
+        
+        parameters:
+            graph -- a cause effect graph (either as a true Graph or a dictionary)
+            
+        returns: test suite serialized to a dictionary
+        """
+        # deserialize the graph in case it is not
+        if type(graph) == dict:
+            graph = graph_from_dict(graph)
+
+        suite: Suite = self.cira.testsuite(ceg=graph)
+
+        # serialize the test suite and return it
+        serialized = suite.to_dict()
+        return serialized
--- src/app.py
+++ src/app.py
@@ -1,23 +1,42 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from api.service import CiraServiceMock
+
+from src.api.service import CiRAService, CiRAServiceImpl
+
+import os, dotenv
+dotenv.load_dotenv()
+
+model_env_suffix = '_DEV' if ('DEV_CONTAINER' in os.environ) else ''
+
+model_classification = os.environ[f'MODEL_CLASSIFICATION{model_env_suffix}']
+model_labeling = os.environ[f'MODEL_LABELING{model_env_suffix}']
 
 app = FastAPI()
 PREFIX = "/api"
 
-service = CiraServiceMock(None, None)
+service: CiRAService = CiRAServiceImpl(model_classification=model_classification, model_labeling=model_labeling)
 
 
 class SentenceRequest(BaseModel):
     sentence: str
     language: str = "en"
     labels: list[dict] = []
+    graph: dict = None
 
 
 class ClassificationResponse(BaseModel):
     causal: bool
     confidence: float
+
+class LabelingResponse(BaseModel):
+    labels: list[dict]
+
+class GraphResponse(BaseModel):
+    graph: dict
+
+class TestsuiteResponse(BaseModel):
+    suite: dict
 
 
 @app.get(PREFIX + "/")
@@ -33,10 +52,28 @@ def health():
     return {"status": "up"}
 
 
-@app.post(PREFIX + '/classify', response_model=ClassificationResponse)
+@app.get(PREFIX + '/classify', response_model=ClassificationResponse)
 async def create_classification(req: SentenceRequest):
     causal, confidence = service.classify(req.sentence)
     return ClassificationResponse(causal=causal, confidence=confidence)
+
+
+@app.get(PREFIX + '/label', response_model=LabelingResponse)
+async def create_labels(req: SentenceRequest):
+    labels = service.sentence_to_labels(sentence=req.sentence)
+    return LabelingResponse(labels=labels)
+
+
+@app.get(PREFIX + '/graph', response_model=GraphResponse)
+async def create_graph(req: SentenceRequest):
+    graph = service.sentence_to_graph(sentence=req.sentence, labels=req.labels)
+    return GraphResponse(graph=graph)
+
+
+@app.get(PREFIX + '/testsuite', response_model=TestsuiteResponse)
+async def create_classification(req: SentenceRequest):
+    testsuite = service.graph_to_test(graph=req.graph)
+    return TestsuiteResponse(suite=testsuite)
 
 
 if __name__ == '__main__':
