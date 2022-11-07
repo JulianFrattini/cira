c5b4896cc68caf87ae72a1b4ecbde96aa45f64d8
Andreas Bauer <dev@andreasbauer.org>
2022-11-07 19:16:44 +0100

    Refactor asserts to use HTTP status consts

--- test/app/test_app_isolated.py
+++ test/app/test_app_isolated.py
@@ -1,12 +1,14 @@
 import pytest
 
 from fastapi.testclient import TestClient
+from fastapi import status
 
 from src import app
 from src.api.service import CiraServiceMock
 
 sentence = "If the button is pressed then the system shuts down."
 API_URL = 'http://localhost:8000/api/'
+
 
 @pytest.fixture(scope="module")
 def client() -> TestClient:
@@ -18,7 +20,7 @@ def client() -> TestClient:
 @pytest.mark.unit
 def test_routes(client):
     response = client.get(f'{API_URL}')
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     routes = response.json()
     assert len(routes) >= 4
@@ -28,7 +30,7 @@ def test_routes(client):
 def test_health(client):
     response = client.get(f'{API_URL}health')
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'status': 'up'}
 
 
@@ -37,7 +39,7 @@ def test_classification(client):
     response = client.get(
         f'{API_URL}classify', json={"sentence": sentence})
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'causal': True, 'confidence': 0.99}
 
 
@@ -46,7 +48,7 @@ def test_labels(client):
     response = client.get(
         f'{API_URL}label', json={"sentence": sentence})
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'labels': [
         {'id': 'L1', 'name': 'Variable', 'begin': 10, 'end': 20, 'parent': None}]}
 
@@ -56,7 +58,7 @@ def test_graph(client):
     response = client.get(
         f'{API_URL}graph', json={"sentence": sentence})
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'graph': {
         'nodes': [
             {'id': 'c', 'variable': 'the button', 'condition': 'is pressed'},
@@ -72,7 +74,7 @@ def test_suite(client):
     response = client.get(
         f'{API_URL}testsuite', json={"sentence": sentence, "graph": None})
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'suite': {
         'conditions': [{'id': 'c', 'variable': 'the button', 'condition': 'is pressed'}],
         'expected': [{'id': 'c', 'variable': 'the system', 'condition': 'shuts down'}],
