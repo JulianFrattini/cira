7f6817bd458dc6f64419fc8d713ea401abe54c42
Andreas Bauer <dev@andreasbauer.org>
2022-11-11 23:58:31 +0100

    Adjust model download script to check actual files

--- download-models.sh
+++ download-models.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 FILE_MODELS='model/cira-models.zip'
+FILE_CLASSIFIER='model/cira-classifier.bin'
+FILE_LABELER='model/cira-labeler.ckpt'
 
-if [ -e $FILE_MODELS ]; then
+if [ -e $FILE_CLASSIFIER ] && [ -e $FILE_LABELER ] ; then
   echo "$FILE_MODELS already exists"
 else
   echo "Download $FILE_MODELS"
   curl -# "https://zenodo.org/record/7186287/files/cira-models.zip?download=1" -o $FILE_MODELS --create-dirs
   echo "Extract $FILE_MODELS"
-  unzip $FILE_MODELS -d model
+  unzip -o $FILE_MODELS -d model
 fi
