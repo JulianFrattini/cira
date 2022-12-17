a7faef6064cb169450a7e7e81cdde88f48713155
Andreas Bauer <dev@andreasbauer.org>
2022-12-17 18:40:31 +0100

    Change model_locator test to only look for filetype ending

--- test/test_model_locator.py
+++ test/test_model_locator.py
@@ -5,11 +5,10 @@ import model_locator
 @pytest.mark.unit
 def test_locate_labeling_model():
     model_path = model_locator.labeling()
-    assert model_path.endswith('cira-labeler.ckpt')
+    assert model_path.endswith('.ckpt')
 
 
 @pytest.mark.unit
 def test_locate_classification_model():
     model_path = model_locator.classification()
-    assert model_path.endswith('cira-classifier.bin')
-
+    assert model_path.endswith('.bin')
