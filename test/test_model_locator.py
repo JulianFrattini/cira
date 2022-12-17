57ec299fc2bc2fd56ed05d36454c6d48bc277122
Andreas Bauer <dev@andreasbauer.org>
2022-12-17 19:33:40 +0100

    Extend model_locator test

--- test/test_model_locator.py
+++ test/test_model_locator.py
@@ -12,3 +12,9 @@ def test_locate_labeling_model():
 def test_locate_classification_model():
     model_path = model_locator.classification()
     assert model_path.endswith('.bin')
+
+
+@pytest.mark.unit
+def test_locate_error():
+    with pytest.raises(NameError):
+        model_locator.__load_model_env('NOT_EXISTING_ENV')
